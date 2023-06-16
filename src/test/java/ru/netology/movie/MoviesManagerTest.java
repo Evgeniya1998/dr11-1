ee60c43340be2ed448da46017d2a4ca7c8f8cfe4
Evgesha <Slav.novikova2014@gmail.com>
2023-06-16 15:59:29 +0700

    MoviesManagerTest add

--- src/test/java/ru/netology/movie/MoviesManagerTest.java
+++ src/test/java/ru/netology/movie/MoviesManagerTest.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class MoviesManagerTest {
+package ru.netology.movie;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoviesManagerTest {
+    @Test
+    public void testAddOneMovie() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film 1");
+        String[] actual = manager.FindAll();
+        String[] expected = {"Film 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddZeroMovies() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Zero");
+        String[] actual = manager.FindAll();
+        String[] expected = {"Zero"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    
+    @Test
+    public void TestAddAllMovies() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        String[] actual = manager.FindAll();
+        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4", "Film 5"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testfindLastMovies() {
+        MovieManager manager = new MovieManager(5);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        String[] actual = manager.FindLast();
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testfindLastMoviesIfNotFive() {
+        MovieManager manager = new MovieManager(7);
+        manager.addMovie("Film 1");
+        manager.addMovie("Film 2");
+        manager.addMovie("Film 3");
+        manager.addMovie("Film 4");
+        manager.addMovie("Film 5");
+        String[] actual = manager.FindLast();
+        String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
